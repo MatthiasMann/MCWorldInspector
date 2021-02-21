beb8137edc467c366f99d57f079c80a452a9765c
Matthias Mann <MatthiasMann@gmx.de>
2021-02-21 09:18:01 +0100

    added mob spawner panel

--- src/mcworldinspector/MCWorldInspector.java
+++ src/mcworldinspector/MCWorldInspector.java
@@ -107,6 +107,7 @@ public class MCWorldInspector extends javax.swing.JFrame {
         infoPanels.add(new VillagerPanel(workerPool));
         infoPanels.add(new TileEntityTypesPanel(workerPool));
         infoPanels.add(new LootChestPanel(workerPool));
+        infoPanels.add(new MobSpawnerPanel(workerPool));
         infoPanels.add(new BiomeTypesPanel(workerPool));
         infoPanels.add(new StructureTypesPanel(workerPool));
         infoPanels.add(new SimpleThingsPanel(workerPool));
---
+++ src/mcworldinspector/MobSpawnerPanel.java
@@ -0,0 +1,68 @@
+package mcworldinspector;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import mcworldinspector.nbt.NBTTagCompound;
+import mcworldinspector.utils.AsyncExecution;
+
+/**
+ *
+ * @author matthias
+ */
+public class MobSpawnerPanel extends AbstractFilteredPanel<String> {
+    private final ExecutorService executorService;
+    private Set<String> tileEntities = Collections.emptySet();
+
+    public MobSpawnerPanel(ExecutorService executorService) {
+        this.executorService = executorService;
+        setName("Mob Spawners");
+    }
+
+    @Override
+    public void reset() {
+        tileEntities = Collections.emptySet();
+        super.reset();
+    }
+
+    private static final String ID = "minecraft:mob_spawner";
+
+    private static String getSpawnDataID(NBTTagCompound s) {
+        return s.getCompound("SpawnData").getString("id");
+    }
+
+    @Override
+    public void setWorld(World world, WorldRenderer renderer) {
+        super.setWorld(world, renderer);
+        AsyncExecution.submitNoThrow(executorService, () -> {
+            return world.chunks().flatMap(c -> c.getTileEntities(ID))
+                    .map(MobSpawnerPanel::getSpawnDataID)
+                    .filter(Objects::nonNull)
+                    .distinct()
+                    .collect(Collectors.toCollection(TreeSet::new));
+        }, result -> {
+            tileEntities = result;
+            buildListModel();
+        });
+    }
+
+    @Override
+    protected List<String> filteredList(String filter) {
+        return filteredStringList(tileEntities, filter);
+    }
+
+    @Override
+    protected Stream<? extends WorldRenderer.HighlightEntry> createHighlighter(List<String> selected) {
+        return world.getChunks().parallelStream()
+                .filter(chunk -> chunk.getTileEntities(ID).map(MobSpawnerPanel::getSpawnDataID).anyMatch(selected::contains))
+                .map(chunk -> new TileEntityTypesPanel.TileEntityHighlightEntry(chunk,
+                        "Tile entity details for ",
+                        t -> selected.contains(getSpawnDataID(t)),
+                        e -> MCItem.createChestView(world, e, null)));
+    }
+}
